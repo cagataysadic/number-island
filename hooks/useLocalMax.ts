16017db710cb4003066803eb0d2d5afec551cef6
cagataysadic <ccagatays@hotmail.com>
2025-05-31 18:16:59 +0300

    minor adjustment

--- hooks/useLocalMax.ts
+++ hooks/useLocalMax.ts
@@ -31,14 +31,14 @@ function useLocalMax() {
 
       return neighbors.every(n => num > n);
     } else if (gameDifficultyRef.current === Difficulty.Hard) {
-      const northwest = index - rowSize >= 0 && index % rowSize !== 0 ? nums[index - rowSize - 1] : -1;
       const north = index - rowSize >= 0 ? nums[index - rowSize] : -1;
+      const west = index % rowSize !== 0 ? nums[index - 1] : -1;
+      const south = index + rowSize < 36 ? nums[index + rowSize] : -1;
+      const east = (index + 1) % rowSize !== 0 ? nums[index + 1] : -1;
+      const northwest = index - rowSize >= 0 && index % rowSize !== 0 ? nums[index - rowSize - 1] : -1;
       const northeast = index - rowSize >= 0 && (index + 1) % rowSize !== 0 ? nums[index - rowSize + 1] : -1;
       const southwest = index + rowSize < 36 && index % rowSize !== 0 ? nums[index + rowSize - 1] : -1;
-      const south = index + rowSize < 36 ? nums[index + rowSize] : -1;
       const southeast = index + rowSize < 36 && (index + 1) % rowSize !== 0 ? nums[index + rowSize + 1] : -1;
-      const west = index % rowSize !== 0 ? nums[index - 1] : -1;
-      const east = (index + 1) % rowSize !== 0 ? nums[index + 1] : -1;
 
       const neighbors = [northwest, north, northeast, southwest, south, southeast, west, east].filter(n => n !== -1);
 
